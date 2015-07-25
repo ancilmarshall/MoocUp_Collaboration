5106743fc38b587d82f75dfdef0c4e98ad50fc32
Retina <Retina@MacBook-Pro-de-Retina.local>
2015-07-25 19:54:43 +0200

    Add the Udacity Api Manager

---
+++ MoocApiParseApp/MoocApiParseApp/UdacityApiManager.swift
@@ -0,0 +1,21 @@
+//
+//  UdacityApiManager.swift
+//  MoocApiParseApp
+//
+//  Created by Retina on 25/07/2015.
+//  Copyright (c) 2015 Ancil Marshall. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class UdacityApiManager
+{
+
+    let ServerScheme = "https"
+    let ServerHost = "api.coursera.org"
+    let ServerPath = "/api/catalog.v1/"
+    let kCourseClassName = "MUCourse"
+    let kMoocName = "Coursera"
+
+}
