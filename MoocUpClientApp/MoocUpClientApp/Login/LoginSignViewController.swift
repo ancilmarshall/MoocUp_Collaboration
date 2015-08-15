0236c24cd7659a6ebb9ac1fc943ce903025948b1
horla <h0rl4.zone@gmail.com>
2015-08-15 12:25:08 +0200

    LoginSignViewController creation du controlleur de vue sign In

---
+++ MoocUpClientApp/MoocUpClientApp/Login/LoginSignViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginSignViewController.swift
+//  MoocUpClientApp
+//
+//  Created by horla on 15/08/2015.
+//  Copyright (c) 2015 Ancil Marshall. All rights reserved.
+//
+
+import UIKit
+
+class LoginSignViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
