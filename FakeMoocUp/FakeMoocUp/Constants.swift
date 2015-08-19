cc0b2af627632a1ed90e4774b318c08e2568917e
Ancil <ancilm2000@yahoo.com>
2015-08-19 09:47:31 +0200

    Remove FakeMoocUp app from the feature-login branch

--- FakeMoocUp/FakeMoocUp/Constants.swift
+++
@@ -1,8 +0,0 @@
-
-import Foundation
-
-struct Constants {
-    
-    static let courseraURL = NSURL(string: "https://api.coursera.org/api/catalog.v1/courses?fields=name,language,shortDescription,recommendedBackground")!
-
-}
