9e5ef71850affd0e9f9d0686f4a5c9d2db1c9d55
Ancil <ancilm2000@yahoo.com>
2015-08-19 09:38:33 +0200

    Edit LoginViewController so that code works on iPad in Simulator - Issue found when testing on the iPad using Xcode 6.4 - Temporarily remove the UIDevice code path for iPad

--- MoocUpClientApp/MoocUpClientApp/LoginViewController.swift
+++ MoocUpClientApp/MoocUpClientApp/LoginViewController.swift
@@ -118,9 +118,11 @@ class LoginViewController: UIViewController, UIPageViewControllerDataSource{
         
         // Set the page view controller's bounds using an inset rect so that self's view is visible around the edges of the pages.
         var pageViewRect = self.view.bounds
-        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
-            pageViewRect = CGRectInset(pageViewRect, self.view.frame.width, self.view.frame.size.height)
-        }
+        
+// FIXME: Remvoved by Ancil since this was causing an issue on the iPad in the Simulator
+//        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+//            pageViewRect = CGRectInset(pageViewRect, self.view.frame.width, self.view.frame.size.height)
+//        }
         self.pageViewController!.view.frame = pageViewRect
         self.pageViewController!.didMoveToParentViewController(self)
         
