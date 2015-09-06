9ab008f3ae3a3e87c3eaa78d8b069fcfd0dbe35c
LF <h0rl4.zone@gmail.com>
2015-09-06 04:42:24 +0200

    LoginSignViewController: User Registration avec Parse

--- MoocUpClientApp/MoocUpClientApp/Login/LoginSignViewController.swift
+++ MoocUpClientApp/MoocUpClientApp/Login/LoginSignViewController.swift
@@ -8,22 +8,58 @@
 
 import UIKit
 
-class LoginSignViewController: UIViewController {
+class LoginSignViewController: UIViewController, UITextFieldDelegate {
     
     //widget
-    @IBOutlet weak var textSign: UITextField!
+    //UITextField
+    @IBOutlet weak var userPseudoTextField: UITextField!
+    @IBOutlet weak var userEmailTextField: UITextField!
+    @IBOutlet weak var userPassTextField: UITextField!
     
     @IBOutlet weak var accountLabel: UILabel!
     
+    
+    // Sign Up Button
     @IBAction func signAction(sender: UIButton) {
+            let userPseudo = userPseudoTextField.text
+            let userEmail = userEmailTextField.text
+            let userPassword = userPassTextField.text
+            userPseudoTextField.resignFirstResponder()
+        
+        // Check for empty field
+        if (userPseudo.isEmpty || userEmail.isEmpty || userPassword.isEmpty) {
+            // Display alert message
+            displayAlertMessage("All fields are required")
+            return
+        }
+        
+        // Parse Store Data
+        let user:PFUser = PFUser()
+        
+        user.username = userPseudo
+        user.password = userPassword
+        user.email = userEmail
+    
+        user.signUpInBackgroundWithBlock { (success,error) -> Void in
+            println ("send Ok")
+        }
+        
     }
     
-    // dissmis
+    // dismiss Button
     @IBAction func dissmisButton(sender: UIButton) {
         self.dismissViewControllerAnimated(true, completion: {})
     }
   
+    // dismiss Keyboard
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
     
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        userPseudoTextField.resignFirstResponder()
+        return true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +72,13 @@ class LoginSignViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    // Alerte Message
+    func displayAlertMessage (userMessage:String) {
+        var myAlert = UIAlertController (title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        let okAction = UIAlertAction (title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+        
+    }
     
 
 
